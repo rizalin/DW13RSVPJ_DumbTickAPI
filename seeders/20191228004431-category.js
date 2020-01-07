56d28d81f8d1a8576db354b29d78f016bb8cb3fc
rizalin <rizal.imamnugraha@gmail.com>
2020-01-07 14:57:47 +0700

    Retry Heroku

--- seeders/20191228004431-category.js
+++ seeders/20191228004431-category.js
@@ -7,21 +7,25 @@ module.exports = {
       [
         {
           name: "Sport",
+          img: "https://iptvmasala.com/wp-content/uploads/2019/04/Sports-IPTV-m3u-playlist.jpg",
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: "Music",
+          img: "https://blogmedia.evbstatic.com/wp-content/uploads/wpmulti/sites/3/2018/03/03103805/big-concert-audience-listening-to-music-at-festival-picture-id485343244.jpg",
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: "Science",
+          img: "https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/D7200921_meta_preregistration.jpg?itok=BqEqP2Ki",
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: "Programming",
+          img: "https://www.elegantthemes.com/blog/wp-content/uploads/2017/07/programming-languages-to-learn-for-wordpress-featured-image.png",
           createdAt: new Date(),
           updatedAt: new Date()
         }
